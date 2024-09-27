da10ffd7c75424247b7aa8cd868abf64218bd001
Sujal <90669209+sujalsha@users.noreply.github.com>
2024-09-28 02:53:33 +0530

    Time: 8 ms (63.6%), Space: 8.9 MB (42.16%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        vector<int> count(26, 0);
+
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+
+        for (char c : t) {
+            count[c - 'a']--;
+        }
+
+        for (int i : count) {
+            if (i != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
