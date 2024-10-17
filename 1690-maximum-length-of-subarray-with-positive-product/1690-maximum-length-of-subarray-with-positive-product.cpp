a7f5a2000f8841cba3745cf21e8835c87fb7ce6f
Sujal <90669209+sujalsha@users.noreply.github.com>
2024-10-17 23:29:18 +0530

    Time: 75 ms (78.25%), Space: 60.6 MB (38.5%) - LeetHub

--- 1690-maximum-length-of-subarray-with-positive-product/1690-maximum-length-of-subarray-with-positive-product.cpp
+++ 1690-maximum-length-of-subarray-with-positive-product/1690-maximum-length-of-subarray-with-positive-product.cpp
@@ -5,15 +5,26 @@ public:
         for (int num : nums) {
             if (num > 0) {
                 max_pos++;
-                max_neg = max_neg > 0 ? max_neg + 1 : 0;
-            } else if (num < 0) {
+                if (max_neg > 0) {
+                    max_neg++;
+                }
+            } 
+            else if (num < 0) {
                 int temp = max_pos;
-                max_pos = max_neg > 0 ? max_neg + 1 : 0;
+                if (max_neg > 0) {
+                    max_pos = max_neg + 1;
+                } else {
+                    max_pos = 0;
+                }
                 max_neg = temp + 1;
-            } else {
-                max_pos = max_neg = 0;
+            } 
+            else {
+                max_pos = 0;
+                max_neg = 0;
             }
-            res = max(res, max_pos);
+            if (max_pos > res) {
+                res = max_pos;
+            }
         }
         return res;
     }
